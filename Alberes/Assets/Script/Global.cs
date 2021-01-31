8e67bfc5dbbf18ec76366c8977ecda4e810b008e
Rexmast <viktor.ezersy@gmail.com>
2021-01-31 12:32:18 +0300

    opit8 up

--- Alberes/Assets/Script/Global.cs
+++ Alberes/Assets/Script/Global.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class Global : MonoBehaviour
 {
     public static int k = 3;
+    public static double Bal = 0;
 }
---
+++ Alberes/Assets/Script/TestContro1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestContro1 : MonoBehaviour
+{
+    public Toggle[] ArreaToggleTrue;
+    public double BalPlus;
+    public void Proverka()
+    {
+        for (int i = 0; i < ArreaToggleTrue.Length; i++)
+        {
+            if (ArreaToggleTrue[i].isOn)
+                Global.Bal += BalPlus;
+        }
+        Debug.Log(Global.Bal);
+    }
+
+}
