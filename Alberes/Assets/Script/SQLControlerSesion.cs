811591c058fc2586536904857a2bd2ba0e730e44
NerzulS <78278700+NerzulS@users.noreply.github.com>
2021-05-13 19:47:13 +0300

    kml

--- Alberes/Assets/Script/SQLControlerSesion.cs
+++ Alberes/Assets/Script/SQLControlerSesion.cs
@@ -114,14 +114,26 @@ public class SQLControlerSesion : MonoBehaviour
 
         //проверка на аутизм значения DropdownISPanelQuestionDevase.captionText.ToString()
 
-        DataTable table = Tabel(new SqlDataAdapter("SELECT dbo.Student.Name, dbo.Student.Famil, dbo.Student.Otchestvo FROM            dbo.Student INNER JOIN dbo.Gruppa ON dbo.Student.IdGruppa = dbo.Gruppa.IDGruppa WHERE(dbo.Gruppa.NameGruppa = N'"+ TextDropdown.text + "')", sqlConnection));
+        if (TextDropdown.text == "") { ErrorMeseg("Пожалуйста выберите свою группу."); goto end; }
+
+        DataTable table = Tabel(new SqlDataAdapter("SELECT dbo.Student.Name, dbo.Student.Famil, dbo.Student.Otchestvo FROM            dbo.Student INNER JOIN dbo.Gruppa ON dbo.Student.IdGruppa = dbo.Gruppa.IDGruppa WHERE(dbo.Gruppa.NameGruppa = N'" + TextDropdown.text + "')", sqlConnection)); //DF
         for (int i = 0; i < table.Rows.Count; i++)
 
             DropdownStudent.options.Add(new Dropdown.OptionData(table.Rows[i][0].ToString().Replace(" ","") + " " + table.Rows[i][1].ToString().Replace(" ", "") + " " + table.Rows[i][2].ToString().Replace(" ", ""))) ;
+        end:;
     }
    public void Register()
     {
         //проверка на аутизм значений
+
+        if(TextFieldSurName.text=="" || TextFieldLastName.text=="" || TextFieldFazerName.text=="" || TextFieldPassword.text == ""){ }
+
+        if (TextFieldSurName.text == "") { ErrorMeseg("Поле для имени является пустым, пожалуйста заполните его."); goto end; }
+        if (TextFieldLastName.text == "") { ErrorMeseg("Поле для фамилии является пустым, пожалуйста заполните его."); goto end; }
+        if (TextFieldFazerName.text == "") { ErrorMeseg("Поле для отчества является пустым, пожалуйста заполните его."); goto end; }
+        if (TextFieldPassword.text == "") { ErrorMeseg("Поле для пароля является пустым, пожалуйста заполните его."); goto end; }
+
+
 
         DataTable table = Tabel(new SqlDataAdapter(" SELECT        IDGruppa FROM            dbo.Gruppa WHERE(NameGruppa = N'" + TextDropdown.text + "')", sqlConnection));
         SqlCommand command = new SqlCommand("INSERT INTO [dbo].[Student] ([IdGruppa] ,[Name] ,[Famil] ,[Otchestvo] ,[Password]) VALUES('" + table.Rows[0][0].ToString() + "',N'" + TextFieldSurName.text + "',N'"+ TextFieldLastName.text + "',N'"+ TextFieldFazerName.text + "',N'"+ TextFieldPassword.text + "')", sqlConnection);
@@ -138,27 +150,38 @@ public class SQLControlerSesion : MonoBehaviour
         Global.IDUser = int.Parse(table.Rows[0][0].ToString());
         MenuSing.SetActive(false);
         Menu.SetActive(true);
-
+        end:;
     }
      public void Sing()
     {
-        //проверка на аутизм значений
+        string NoStr, NotExt;
+        NoStr = "Пароль не содержит символов, пожалуйста введите корректный пароль.";
+        NotExt = "Пароль не совпал пажалуйста ведите другой пароль.";
+        //проверка на аутизм значений +
         string[] TextDropdown1 = TextDropdownStudent.text.Split(' ');
+       if (TextDropdownStudent.text == "") { goto NoPas; }
         DataTable table = Tabel(new SqlDataAdapter(" SELECT IdStudent, Password FROM dbo.Student WHERE(Name = N'" + TextDropdown1[0] + "') AND(Famil = N'" + TextDropdown1[1] + "') AND(Otchestvo = N'" + TextDropdown1[2] + "') AND(Password = N'" + TextFieldPasswordSing.text + "')", sqlConnection));
-        if (table.Rows.Count !=0)
+        if (table.Rows.Count != 0)
         {
-            
+
             Global.UserPasword = table.Rows[0][1].ToString();
             Global.IDUser = int.Parse(table.Rows[0][0].ToString());
             MenuSing.SetActive(false);
             Menu.SetActive(true);
         }
-        else
-        {
-            ErrorMeseg("Пароль не совпал пажалуйста ведите другой пароль.");
-        
-            //error
-        }
+        else { goto NotExist; }
+       
+            NoPas:;
+            ErrorMeseg(NoStr);
+            goto end;
+
+            NotExist:;
+            ErrorMeseg(NotExt);
+            goto end;
+
+        //error
+
+        end:;
     }
     void ErrorMeseg(string Show)
     {
